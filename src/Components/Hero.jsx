c81498c059b5e3a50d54249b18a665f9500c1f1c
Mehedi Hasan <mehedih.dev@gmail.com>
2023-03-01 07:46:01 -0400

    init

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 const Hero =()=>{
     return(
-        <nav>
-            <img src="../assets/airbnb-logo.png" alt="Airbnb"/>
-        </nav>
+        <div className='hero'>
+            <nav>
+                <img src="https://th.bing.com/th/id/R.11566b13ebe3fe195137ce2bd1804a69?rik=Og%2bcKTbfN4mhBA&riu=http%3a%2f%2flogos-download.com%2fwp-content%2fuploads%2f2016%2f03%2fAirbnb_logo.png&ehk=QhLUqOjF6HxBvuuxjqpvtKEeCf%2bnDOuAUWx8DInRPOo%3d&risl=&pid=ImgRaw&r=0" alt=""/>
+            </nav>
+            <section className='heroAlbum'>
+                <h1>Here goes the image grid</h1>
+            </section>
+        </div>
     )
 }
 export default Hero
