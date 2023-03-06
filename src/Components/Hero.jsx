89ce26d321adfea2b33aefa7483d32931199d12f
Mehedi Hasan <mehedih.dev@gmail.com>
2023-03-06 08:46:09 -0400

    new files were added

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -3,7 +3,7 @@ const Hero =()=>{
     return(
         <div className='hero'>
             <nav>
-                <img src="https://th.bing.com/th/id/R.11566b13ebe3fe195137ce2bd1804a69?rik=Og%2bcKTbfN4mhBA&riu=http%3a%2f%2flogos-download.com%2fwp-content%2fuploads%2f2016%2f03%2fAirbnb_logo.png&ehk=QhLUqOjF6HxBvuuxjqpvtKEeCf%2bnDOuAUWx8DInRPOo%3d&risl=&pid=ImgRaw&r=0" alt=""/>
+                <img src='../assets/airbnb-logo.png' alt=""/>
             </nav>
             <section className='heroAlbum'>
                 <h1>Here goes the image grid</h1>
